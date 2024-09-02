7320b7afa57eea21fd33471024dc4661c20f1c70
Johan Martin Aarstein <johan@aarstein.media>
2024-09-02 22:25:44 +0200

    stash

--- scripts/am-gdpr.js
+++ scripts/am-gdpr.js
@@ -1,3 +1,5 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 (function (exports) {
     'use strict';
 
@@ -499,7 +501,7 @@
 
     const UPDATE_ON_CONNECTED = Symbol('UPDATE_ON_CONNECTED');
     if (isServer()) {
-        global.HTMLElement = class EmptyHTMLElement {
+        global.HTMLElement = class EmptyHTMLElement extends HTMLElement {
         };
     }
     let EnhancedElement = class EnhancedElement extends HTMLElement {
@@ -542,6 +544,7 @@
             }
         }
     };
+    EnhancedElement.observedProperties = [];
 
     class AMGDPR extends EnhancedElement {
         connectedCallback() {
