15dd8567d66f3ccb4d6967d9570e4d7ba011119e
Johan Martin Aarstein <johan@aarstein.media>
2024-10-15 21:36:50 +0200

    wp-coding-standards

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,5 @@
 <?php
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Returns the plugin path to a specified file.
@@ -7,26 +7,24 @@ defined('ABSPATH') || exit;
  * @param string $filename The specified file.
  * @return string
  */
-function aamd_cookies_get_path($path = '')
-{
-  $path = preg_replace('/\.[^.]*$/', '', ltrim($path, '/')) . '.php';
-  return AAMD_COOKIES_PATH . $path;
+function aamd_cookies_get_path( $path = '' ) {
+	$path = preg_replace( '/\.[^.]*$/', '', ltrim( $path, '/' ) ) . '.php';
+	return AAMD_COOKIES_PATH . $path;
 }
 
 /**
  * Includes a file within the plugins includes folder
  *
  * @param string $filename The specified file.
- * @param mixed $arg (optional)
+ * @param mixed  $arg (optional)
  * @return void
  */
-function aamd_cookies_include($path = '', $args = null)
-{
-  $path = aamd_cookies_get_path('includes/' . ltrim($path, '/'));
-  if (file_exists($path)) {
-    $args;
-    include_once $path;
-  }
+function aamd_cookies_include( $path = '', $args = null ) {
+	$path = aamd_cookies_get_path( 'includes/' . ltrim( $path, '/' ) );
+	if ( file_exists( $path ) ) {
+		$args;
+		include_once $path;
+	}
 }
 
 /**
@@ -35,23 +33,20 @@ function aamd_cookies_include($path = '', $args = null)
  * @param   string $str The string to convert.
  * @return  string
  */
-function aamd_idify($str = '')
-{
-  return str_replace(['][', '[', ']'], ['-', '-', ''], strtolower($str));
+function aamd_idify( $str = '' ) {
+	return str_replace( array( '][', '[', ']' ), array( '-', '-', '' ), strtolower( $str ) );
 }
 
-function aamd_use_id()
-{
-  $str = wp_rand();
-  return aamd_idify(md5($str));
+function aamd_use_id() {
+	$str = wp_rand();
+	return aamd_idify( md5( $str ) );
 }
 
 /**
  * Converts slug to snake_case
+ *
  * @param string $str
- * 
  */
-function aamd_snakeify($str)
-{
-  return strtolower(preg_replace("/[\-]/", "_", $str));
+function aamd_snakeify( $str ) {
+	return strtolower( preg_replace( '/[\-]/', '_', $str ) );
 }
--- svn/trunk/am-cookies.php
+++ svn/trunk/am-cookies.php
@@ -2,7 +2,7 @@
 
 /**
  * AM Cookies for WordPress
- * 
+ *
  * @package AM Cookies
  * @author Aarstein Media
  *
@@ -19,80 +19,78 @@
  * Text Domain:       am-cookies
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
-if (!class_exists('AAMD_Cookies'))
-{
-  class AAMD_Cookies
-  {
-    public $version;
+if ( ! class_exists( 'AAMD_Cookies' ) ) {
+	class AAMD_Cookies {
 
-    /**
-     * Constructor.
-     *
-     * @param   void
-     * @return  void
-     */
-    public function __construct()
-    {
-      $this->version = '1.0.1';
-    }
+		public $version;
 
-    /**
-     * Sets up AM GDPR plugin
-     * @return void
-     */
-    public function initialize()
-    {
+		/**
+		 * Constructor.
+		 *
+		 * @param   void
+		 * @return  void
+		 */
+		public function __construct() {
+			$this->version = '1.0.1';
+		}
 
-      // Define constants
-      define('AAMD_COOKIES_PATH', plugin_dir_path(__FILE__));
-      define('AAMD_COOKIES_SLUG', plugin_basename(__DIR__));
-      define('AAMD_COOKIES_BASENAME', plugin_basename(__FILE__));
-      define('AAMD_COOKIES_VERSION', $this->version);
-      define('AAMD_COOKIES_URL', plugin_dir_url(__FILE__));
+		/**
+		 * Sets up AM GDPR plugin
+		 *
+		 * @return void
+		 */
+		public function initialize() {
 
-      // Include utility functions
-      include_once AAMD_COOKIES_PATH . 'includes/functions.php';
+			// Define constants
+			define( 'AAMD_COOKIES_PATH', plugin_dir_path( __FILE__ ) );
+			define( 'AAMD_COOKIES_SLUG', plugin_basename( __DIR__ ) );
+			define( 'AAMD_COOKIES_BASENAME', plugin_basename( __FILE__ ) );
+			define( 'AAMD_COOKIES_VERSION', $this->version );
+			define( 'AAMD_COOKIES_URL', plugin_dir_url( __FILE__ ) );
 
-      aamd_cookies_include('enqueue-scripts');
-      aamd_cookies_include('rest-api');
-      if (is_admin()) {
-        aamd_cookies_include('admin');
-      }
+			// Include utility functions
+			include_once AAMD_COOKIES_PATH . 'includes/functions.php';
 
-      add_option('aamd_cookies_google_id', null);
-      add_option('aamd_cookies_meta_id', null);
-      add_option('aamd_cookies_snap_id', null);
-      add_option('aamd_cookies_tiktok_id', null);
+			aamd_cookies_include( 'enqueue-scripts' );
+			aamd_cookies_include( 'rest-api' );
+			if ( is_admin() ) {
+				aamd_cookies_include( 'admin' );
+			}
 
-      add_option('aamd_cookies_align', 'bottom-left');
-      add_option('aamd_cookies_align_mini', 'bottom-left');
-      add_option('aamd_cookies_format', 'box');
-      add_option('aamd_cookies_font_family', 'sans-serif');
-      add_option('aamd_cookies_color', '#000000');
-      add_option('aamd_cookies_accent_color', '#ffffff');
-      add_option('aamd_cookies_background_color', '#ffffff');
-      add_option('aamd_cookies_border_width', 2);
-      add_option('aamd_cookies_text', null);
-      add_option('aamd_cookies_wp_privacy_policy_url', 'privacy-policy');
-    }
-  }
+			add_option( 'aamd_cookies_google_id', null );
+			add_option( 'aamd_cookies_meta_id', null );
+			add_option( 'aamd_cookies_snap_id', null );
+			add_option( 'aamd_cookies_tiktok_id', null );
+
+			add_option( 'aamd_cookies_align', 'bottom-left' );
+			add_option( 'aamd_cookies_align_mini', 'bottom-left' );
+			add_option( 'aamd_cookies_format', 'box' );
+			add_option( 'aamd_cookies_font_family', 'sans-serif' );
+			add_option( 'aamd_cookies_color', '#000000' );
+			add_option( 'aamd_cookies_accent_color', '#ffffff' );
+			add_option( 'aamd_cookies_background_color', '#ffffff' );
+			add_option( 'aamd_cookies_border_width', 2 );
+			add_option( 'aamd_cookies_text', null );
+			add_option( 'aamd_cookies_wp_privacy_policy_url', 'privacy-policy' );
+		}
+	}
 }
 
 
 /**
  * Main function, to initialize plugin
+ *
  * @return AAMD_Cookies
  */
-(function()
-{
-  global $aamd_cookies;
+( function () {
+	global $aamd_cookies;
 
-  if (!isset($aamd_cookies)) {
-    $aamd_cookies = new AAMD_Cookies();
-    $aamd_cookies->initialize();
-  }
+	if ( ! isset( $aamd_cookies ) ) {
+		$aamd_cookies = new AAMD_Cookies();
+		$aamd_cookies->initialize();
+	}
 
-  return $aamd_cookies;
-})();
+	return $aamd_cookies;
+} )();
