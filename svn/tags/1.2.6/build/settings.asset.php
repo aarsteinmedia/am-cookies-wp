5e22c3786e68609246c559971fc167f24d09e3a2
Johan Martin Aarstein <johan@aarstein.media>
2024-12-10 16:31:38 +0100

    stash

---
+++ svn/tags/1.2.6/build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bc6eb77e11548fa9d1cf');
